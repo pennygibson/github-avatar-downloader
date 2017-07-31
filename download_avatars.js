d7e99d4ff806f621064c393aea8cc30c2c79db88
pennygibson <cenneboulet@yahoo.ca>
2017-07-31 20:52:45 +0000

    complete first assignment

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,31 @@
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+  var GITHUB_USER = "pennygibson";
+  var GITHUB_TOKEN = "2cb5712cd7e2523d398b7cf0174f86abd8ca2a7d";
+
+function getRepoContributors(repoOwner, repoName, cb){
+  var requestURL = 'https://'+GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
+
+   var options = {
+    url: requestURL,
+    headers: {"User-Agent":"GitHub Avatar Downloader - Student Project"}
+  }
+
+  request(options, function (error, response, body){
+
+     var data = JSON.parse(body);
+
+     cb(error, data)
+
+
+});
+}
+
+getRepoContributors("jquery", "jquery", function(error, data) {
+  for(var i = 0; i < data.length; i++){
+    console.log(data[i].avatar_url);
+  }
+});
